5b82fdf632c368fa2ca3e6ac8121315faf9c492b
Sparsh Goyal <sparshg05@gmail.com>
2023-01-03 17:04:38 +0530

    added backend url

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const url = 'https://mesparshg05-portfolio.adaptable.app/';
+
 export const getUser = () => async(dispatch)=>{
     try{
         dispatch({
-            type: "GET_USER_REQUEST",
+            type: "GET_USER_REQUEST",   
         });
 
-        const {data} = await axios.get("/api/v1/user");
+        const {data} = await axios.get(url + "/api/v1/user");
 
         dispatch({
             type: "GET_USER_SUCCESS",
@@ -29,7 +31,7 @@ export const login = (email,password) => async(dispatch)=>{
             type: "LOGIN_REQUEST",
         });
 
-        const {data} = await axios.post("/api/v1/login",{
+        const {data} = await axios.post(url + "/api/v1/login",{
             email,
             password,
         },{
@@ -59,7 +61,7 @@ export const logout = () => async(dispatch)=>{
             type: "LOGOUT_REQUEST",
         });
 
-        const {data} = await axios.get("/api/v1/logout");
+        const {data} = await axios.get(url + "/api/v1/logout");
 
         dispatch({
             type: "LOGOUT_SUCCESS",
@@ -82,7 +84,7 @@ export const loadUser = () => async(dispatch)=>{
             type: "LOAD_USER_REQUEST",
         });
 
-        const {data} = await axios.get("/api/v1/me");
+        const {data} = await axios.get(url + "/api/v1/me");
 
         dispatch({
             type: "LOAD_USER_SUCCESS",
@@ -105,7 +107,7 @@ export const updateUser = (name,email,password,skills,about) => async(dispatch)=
             type: "UPDATE_USER_REQUEST",
         });
 
-        const {data} = await axios.put("/api/v1/admin/update",{
+        const {data} = await axios.put(url + "/api/v1/admin/update",{
             name,email,password,skills,about
         },{
             headers:{
@@ -134,7 +136,7 @@ export const addTimeline = (title,description,date) => async(dispatch)=>{
             type: "ADD_TIMELINE_REQUEST",
         });
 
-        const {data} = await axios.post("/api/v1/admin/timeline/add",{
+        const {data} = await axios.post(url + "/api/v1/admin/timeline/add",{
             title,description,date
         },{
             headers:{
@@ -163,7 +165,7 @@ export const deleteTimeline = (id) => async(dispatch)=>{
             type: "DELETE_TIMELINE_REQUEST",
         });
 
-        const {data} = await axios.delete(`/api/v1/admin/timeline/${id}`);
+        const {data} = await axios.delete(url + `/api/v1/admin/timeline/${id}`);
 
         dispatch({
             type: "DELETE_TIMELINE_SUCCESS",
@@ -186,7 +188,7 @@ export const addProject = (title,url,image,description,techStack) => async(dispa
             type: "ADD_PROJECT_REQUEST",
         });
 
-        const {data} = await axios.post("/api/v1/admin/project/add",{
+        const {data} = await axios.post(url + "/api/v1/admin/project/add",{
             title,url,image,description,techStack
         },{
             headers:{
@@ -215,7 +217,7 @@ export const deleteProject = (id) => async(dispatch)=>{
             type: "DELETE_PROJECT_REQUEST",
         });
 
-        const {data} = await axios.delete(`/api/v1/admin/project/${id}`);
+        const {data} = await axios.delete(url + `/api/v1/admin/project/${id}`);
 
         dispatch({
             type: "DELETE_PROJECT_SUCCESS",
@@ -238,7 +240,7 @@ export const contactUs = (name,email,message) => async(dispatch)=>{
             type: "CONTACT_US_REQUEST",
         });
 
-        const {data} = await axios.post("/api/v1/contact",{
+        const {data} = await axios.post(url + "/api/v1/contact",{
             name,email,message
         },{
             headers:{
